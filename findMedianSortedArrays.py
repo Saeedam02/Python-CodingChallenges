424ad6aca2d3308f3e2c1be79726420ef76b2965
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-10-26 20:20:11 +0330

    Adding Approach 3: A Better Binary Search

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -88,3 +88,37 @@ nums1 = [1,3]
 nums2 = [2]
 v = RecursiveSolution()
 print(v.findMedianSortedArrays(nums1,nums2))
+
+#Approach 3: A Better Binary Search
+class BetterSolution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partitionA = (left + right) // 2
+            partitionB = (m + n + 1) // 2 - partitionA
+
+            maxLeftA = float('-inf') if partitionA == 0 else nums1[partitionA - 1]
+            minRightA = float('inf') if partitionA == m else nums1[partitionA]
+            maxLeftB = float('-inf') if partitionB == 0 else nums2[partitionB - 1]
+            minRightB = float('inf') if partitionB == n else nums2[partitionB]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
+                else:
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partitionA - 1
+            else:
+                left = partitionA + 1
+
+nums1 = [1,3]
+nums2 = [2,2,5,6]
+x = BetterSolution()
+print(x.findMedianSortedArrays(nums1,nums2))
